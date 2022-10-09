de4e5e11ef86f69004f4e84ef744b97f04d2a5a1
WBT <WBT@users.noreply.github.com>
2022-10-09 01:00:57 -0400

    Copy types from Coinbase advanced API docs

--- source/frontend/src/App.tsx
+++ source/frontend/src/App.tsx
@@ -14,6 +14,64 @@ interface DataForDisplay {
   TxRows: TxRowData[];
 }
 
+interface TokenTransfer {
+  //https://docs.cloud.coinbase.com/node/reference/advanced-api-reference#tokentransfer
+  tokenAddress: string;
+  tokenType:  "erc20" | "erc721";
+  from: string;
+  to: string;
+  value: string; // For ERC-20, gives quantity of tokens transferred. For ERC-721, gives list of token IDs of the token transferred
+  transactionHash: string;
+  transactionIndex: string;
+}
+
+interface InternalTransaction {
+  //https://docs.cloud.coinbase.com/node/reference/advanced-api-reference#internaltransaction
+  traceType: string; //Type of internal transaction, e.g. CREATE, CALL, CALLCODE, DELEGATECALL, SUICIDE
+  from: string;
+  to: string;
+  value: string; //The value in native blockchain currency.
+  gasLimit?: string
+}
+interface Transaction {
+  //https://docs.cloud.coinbase.com/node/reference/advanced-api-reference#transaction
+  transactionHash:	string;
+  transactionIndex:	string;
+  from:	string; //The origin address.
+  to:	string; //The destination address.
+  value:	string; //The value in native blockchain currency.
+  gasLimit:	string; //The maximum gas limit of a transaction.
+  gasPrice:	string; //Transaction's cost per unit of gas in native blockchain currency.
+  gasUsed:	string; //Amount of gas actually used in transaction.
+  cumulativeGasUsed:	string;	//Total amount of gas used in the block of the transaction.
+  status:	string; //"1": Success, "0": Fail, Other return codes: Unknown.
+  input:	string;
+  nonce:	string;
+  blockHash:	string;
+  blockNumber:	string;
+  blockTimestamp:	string;
+  internalTransactions?:	InternalTransaction[];
+  tokenTransfers?:	TokenTransfer[];
+}
+interface BlockTransaction {
+  //https://docs.cloud.coinbase.com/node/reference/advanced-api-reference#blocktransaction
+  blockHash: string;
+  blockNumber: string;
+  blockTimestamp: string;
+  transactions:	Transaction[];
+}
+interface TransactionsByAddress {
+  //https://docs.cloud.coinbase.com/node/reference/advanced-api-reference#transactionsbyaddress
+  blockStart: string;
+  blockEnd: string;
+  blocks: BlockTransaction[];
+}
+
+interface TransactionsByAddressResult {
+  id: number;
+  jsonrpc: string;
+  result: TransactionsByAddress;
+}
 interface paramsForTxByAddress {
   //https://docs.cloud.coinbase.com/node/reference/advanced-api-reference#coinbasecloud_gettransactionsbyaddress
   "address": string; //"0x3cd751e6b0078be393132286c442345e5dc49699",
